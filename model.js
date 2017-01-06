79c435d111b559da19b3b8435a9e10002cd4e614
Yote <yotewach@corp.ais900.org>
2017-01-06 10:54:12 +0700

    update mock schema

--- model.js
+++ model.js
@@ -1,77 +1,50 @@
 var Result = function () {
-    this.cpu = [];
-    this.diskio = [];
-    this.diskusage = [];
-    this.httpget = [];
-    this.memory = [];
-    this.netWorkBandwidth = [];
-    this.netWorkconcurrence = [];
+    this.result = [];
 };
 
 Result.prototype = {
     query: function (obj) {
 
-        var that = this;
+        var that = this.result;
 
-        var cpu = [],
-            diskio = [],
+        var diskio = [],
             diskusage = [],
-            httpget = [],
-            memory = [],
-            netWorkBandwidth = [],
-            netWorkconcurrence = [];
+            nwbandwidth = [];
 
-        that.cpu = function () {
-            var serviceCpu = new Cpu(97.98, 0.00, 0.00, 0.00, 1.01, 1.01);
-            cpu.push(new Data(serviceCpu, "", 1482295537655));
-            return cpu;
-        } ();
+        //call service cpu
+        var serviceCpu = new Cpu(97.98, 0.00, 0.00, 0.00, 1.01, 1.01);
+        that.push(new Data("cpu", serviceCpu, "", 1482295537655));
 
-        that.diskio = function () {
-            var serviceDiskIo = new DiskIo(0.00, 0.00, 0.00);
-            var diskNames = ["sta", "stb"];
+        //call service disk io
 
-            for (var i = 0; i < diskNames.length; i++) {
-                diskio[i] = { [diskNames[i]]: serviceDiskIo };
-            }
+        var diskNames = ["sta", "stb"];
 
-            return new Data(diskio, "", 1482295537655);
-        } ()
+        for (var i = 0; i < diskNames.length; i++) {
+            diskio[i] = new DiskIo(diskNames[i], 0.00, 0.00, 0.00);
+        }
 
-        that.diskusage = function () {
-            var serviceDiskUsage = new DiskUsage(14);
-            var paths = ["/", "/dev/shm", "/vagrant"];
+        that.push(new Data("diskio", diskio, "", 1482295537655));
 
-            for (var i = 0; i <= paths.length; i++) {
-                diskusage[i] = { [paths[i]]: serviceDiskUsage };
-            }
+        //call service disk usage         
+        var paths = ["/", "/dev/shm", "/vagrant"];
 
-            return new Data(diskusage, "", 1482295537655);
-        } ()
+        for (var i = 0; i < paths.length; i++) {
+            diskusage[i] = new DiskUsage(paths[i], 14);
+        }
 
-        that.httpget = function () {
-            var serviceHttpGet = new HttpGet(4443, 200, "OK", 104, "http://www.picture-organic-clothing.com/wp-content/uploads/2016/08/exp-banner.jpg");
-            httpget.push(new Data(serviceHttpGet, "", 1482295537655));
-            return httpget;
-        } ()
+        that.push(new Data("diskusage", diskusage, "", 1482295537655));
 
-        that.memory = function () {
-            var serviceMemory = new Memory("500072", "39776", "87736", "38.68", "372560", "17.149999999999999", "0", "603576", "231016")
-            memory.push(new Data(serviceMemory, "", 1482295537655));
-            return memory;
-        } ()
+        var serviceMemory = new Memory("500072", "39776", "87736", "38.68", "372560", "17.149999999999999", "0", "603576", "231016")
+        that.push(new Data("memory", serviceMemory, "", 1482295537655));
 
-        that.netWorkBandwidth = function () {
-            var serviceNetWorkBandwidth = new NetWorkBandwidth("0.39", "0.35");
-            netWorkBandwidth.push(new Data(serviceNetWorkBandwidth, "", 1482295537655));
-            return netWorkBandwidth;
-        } ()
+        var devices = ["eth0", "lo"];
+        for (var i = 0; i < devices.length; i++) {
+            nwbandwidth[i] = new NetWorkBandwidth(devices[i], "0.39", "0.35");
+        }
+        that.push(new Data("nwbandwidth", nwbandwidth, "", 1482295537655));
 
-        that.netWorkconcurrence = function () {
-            var serviceNetworkConcurrence = new NetworkConcurrence(1, 1, 0, 16, 0, 1);
-            netWorkconcurrence.push(new Data(serviceNetworkConcurrence, "", 1482295537655));
-            return netWorkconcurrence;
-        } ()
+        var serviceNetworkConcurrence = new NetworkConcurrence(1, 1, 0, 16, 0, 1);
+        that.push(new Data("nwconcurrence", serviceNetworkConcurrence, "", 1482295537655));
 
         return that;
     }
@@ -79,8 +52,9 @@ Result.prototype = {
 }
 
 
-function Data(obj, error, timestamp) {
-    this.data = obj;
+function Data(interfaceType, data, error, timestamp) {
+    this.interfaceType = interfaceType;
+    this.data = data;
     this.error = error;
     this.timestamp = timestamp;
 }
@@ -94,13 +68,15 @@ function Cpu(idle, iowait, nice, steal, system, user) {
     this.user = user;
 }
 
-function DiskIo(read, util, write) {
+function DiskIo(device, read, util, write) {
+    this.device = device;
     this.read = read;
     this.util = util;
     this.write = write;
 }
 
-function DiskUsage(usage) {
+function DiskUsage(path, usage) {
+    this.path = path;
     this.usage = usage;
 };
 
@@ -123,7 +99,8 @@ function Memory(actualFree, buffers, cached, comparePercentUsed, free, percentUs
     this.used = used;
 }
 
-function NetWorkBandwidth(rxkBps, txkBps) {
+function NetWorkBandwidth(device, rxkBps, txkBps) {
+    this.device = device;
     this.rxkBps = rxkBps;
     this.txkBps = txkBps;
 }
